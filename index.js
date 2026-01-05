c833b8e866c96db2f60311a9547afae7f8d3c1ba
Mahfuz Shakib <mahfuzshakib301@gmail.com>
2026-01-05 17:29:48 +0600

    server side code added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const { MongoClient, ServerApiVersion, CURSOR_FLAGS, ObjectId } = require("mongodb");
 
 const app = express();
@@ -9,9 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb+srv://simpleDbUser:OzfNDgDrL5W1LrAT@cluster0.pr7icaj.mongodb.net/?appName=Cluster0";
-// const uri = "mongodb+srv://mshakib2301:VIt0bhOd4LZBVVx5@cluster0.pr7icaj.mongodb.net/?appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pr7icaj.mongodb.net/?appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,15 +32,15 @@ async function run() {
     // user related apis
 
     app.get("/user", async (req, res) => {
-    //   const email = req.query.email;
-    //   const query = {};
-    //   if (email) {
-    //     query.email = email;
-    //   }
-    //   console.log(query);
+      //   const email = req.query.email;
+      //   const query = {};
+      //   if (email) {
+      //     query.email = email;
+      //   }
+      //   console.log(query);
       const cursor = myInformation.find();
       const result = await cursor.toArray();
-    //   console.log(result);
+      //   console.log(result);
       res.send(result);
     });
 
@@ -53,11 +53,7 @@ async function run() {
           return res.status(400).json({ error: "Missing id or update body" });
         }
 
-        const result = await myInformation.updateOne(
-          { _id: id },
-          { $set: update },
-          { upsert: false }
-        );
+        const result = await myInformation.updateOne({ _id: id }, { $set: update }, { upsert: false });
 
         if (result.matchedCount === 0) {
           return res.status(404).json({ error: "Document not found" });
